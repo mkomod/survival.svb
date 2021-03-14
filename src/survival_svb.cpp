84acbc58a3be0cc4e17cf0872eb58d0f74c8b51a
mkomod <mkomodromos01@gmail.com>
2021-03-15 00:16:35 +0200

    fix: bugs in functions

--- src/survival_svb.cpp
+++ src/survival_svb.cpp
@@ -1,5 +1,10 @@
 #include "RcppArmadillo.h"
+#include "RcppEnsmallen.h"
+// [[Rcpp::depends(RcppEnsmallen)]]
+// [[Rcpp::depends(RcppArmadillo)]]
 
+
+// [[Rcpp::export]]
 arma::vec
 initialise_P(arma::vec m, arma::vec s, arma::mat X)
 {
@@ -9,51 +14,44 @@ initialise_P(arma::vec m, arma::vec s, arma::mat X)
     for (int i = 0; i < n; ++i) {
 	arma::rowvec x = X.row(i);
 	double xSx = (x * arma::diagmat(s) * x.t()).eval().at(0, 0);
-	P(i) = dot(x, m) + 1/2 + xSx;
+	P(i) = dot(x, m) + 1.0/2 * xSx;
     }
 
     return P;
 }
 
+// [[Rcpp::export]]
 arma::vec
 sub_from_P(arma::vec P, double m, double s, arma::vec x_j) 
 {
-    for (int i = 0; i < P.n_rows; ++i) {
-	P(i) -= x_j(i) * m + 1/2 * pow(s * x_j(i), 2);
-    }
-    return P;
+    return P -= x_j * m + 1.0/2 * pow(s * x_j, 2);
 }
 
+// [[Rcpp::export]]
 arma::vec
 add_to_P(arma::vec P, double m, double s, arma::vec x_j) 
 {
-    for (int i = 0; i < P.n_rows; ++i) {
-	P(i) += x_j(i) * m + 1/2 * pow(s * x_j(i), 2);
-    }
-    return P;
+    return P += x_j * m + 1.0/2 * pow(s * x_j, 2);
 }
 
 
 
+// [[Rcpp::export]]
 double
 update_likelihood(double m, double s, double omega, arma::vec T, 
 	arma::vec x_j, arma::vec P)
 {
-    double res = 0;
-    for (int i = 0; i < x_j.n_rows; ++i) {
-	res += omega * T(i) * exp(x_j(i) * m + 1/2 * pow(x_j(i) * s, 2) + P(i)) 
-	    - m * x_j(i);
-    }
-    return res;
+    return dot(omega * T, exp(x_j * m + 1.0/2 * pow(x_j * s, 2) + P) - m * x_j);
 }
 
 
+// [[Rcpp::export]]
 double
 update_variational(double m, double s, double l)
 {
     double m_s = m / s;
-    return l * s * sqrt(2 / PI) * exp( - pow(m_s, 2)) +
-	l * m * (1 - R::pnorm(-m_s, 0, 1, 0, 0));
+    return l * s * sqrt(2.0 / PI) * exp(-pow(m_s, 2)) +
+	l * m * (1.0 - 2.0 * R::pnorm(-m_s, 0, 1, 1, 0));
 }
 
 
@@ -92,12 +90,12 @@ fit(arma::colvec T, arma::mat X, double omega)
     for (int j = 0; j < X.n_cols; ++j) {
     // opt m
 	P = sub_from_P(P, m(j), s(j), X.col(j));
-	m(j) = update_m();
+	// m(j) = update_m();
 	P = add_to_P(P, m(j), s(j), X.col(j));
 
 	// opt s
 	P = sub_from_P(P, m(j), s(j), X.col(j));
-	s(j) = update_s();
+	// s(j) = update_s();
 	P = add_to_P(P, m(j), s(j), X.col(j));
 
 	// opt g
