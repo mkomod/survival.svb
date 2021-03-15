dc425b995925a1b469f53b6a637de1efe2e3839d
mkomod <mkomodromos01@gmail.com>
2021-03-15 20:11:31 +0200

    init: exp header file

---
+++ src/exponential.hpp
@@ -0,0 +1,11 @@
+#ifndef _EXPONENTIAL_H
+#define _EXPONENTIAL_H
+
+#include "RcppArmadillo.h"
+#include "utils.hpp"
+
+class ExponentialMean;
+class ExponentialVariance;
+class ExponentialWeights;
+
+#endif
