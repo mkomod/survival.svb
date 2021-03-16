22abb24b6c6e956062dacdfef8df3bc619a251d8
mkomod <mkomodromos01@gmail.com>
2021-03-16 21:58:55 +0200

    add: exponential model

--- src/exponential.cpp
+++ src/exponential.cpp
@@ -1,79 +1,99 @@
 #include "exponential.hpp"
 
-
-class ExponentialMean {
-    private:
-	arma::colvec P;
-	arma::colvec x_j;
-	arma::colvec T;
-	double omega;
-	double sigma;
-	double lambda;
-
-
-    public:
-	ExponentialMean(const arma::colvec &P_, const arma::colvec &x_j_, 
-		const arma::colvec &T_, double omega_, double sigma_, 
-		double lambda_) :
-	    P(P_), x_j(x_j_), T(T_), omega(omega_), sigma(sigma_), 
-	    lambda(lambda_) {};
-
-	double Evaluate(const double mu) {
-	    return sum(omega * T % normal_mgf(x_j, mu, sigma) % P - mu * x_j) +
-		lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2)) +
-		lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0));
-	}
-
-	void Gradient(const double mu, double &g) {
-	    g = sum(omega * T % P % normal_mgf(x_j , mu, sigma) % x_j - x_j) -
-		2.0 * lambda * mu/sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2)) +
-		lambda * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) +
-		lambda * 2.0 / sigma * R::dnorm(- mu / sigma, 0, 1, 0);
-	}
+struct kwargs {
+    const double &mu;
+    const double &sigma;
+    const double &lambda;
+    const double &omega;
+    const arma::vec &P;
+    const arma::vec &T; 
+    const arma::vec &x_j;
+    bool verbose;
 };
 
 
-class ExponentialVariance {
-    private:
-	arma::colvec P;
-	arma::colvec x_j;
-	arma::colvec T;
-	double omega;
-	double mu;
-	double lambda;
+double 
+objective_mu_exp(double mu, void* args)
+{
+    kwargs *arg = static_cast<kwargs *>(args);
+    double sigma = arg->sigma;
+    double omega = arg->omega;
+    double lambda = arg->lambda;
+    const arma::vec &P = arg->P;
+    const arma::vec &T = arg->T;
+    const arma::vec x_j = arg->x_j;
+    bool verbose = arg->verbose;
+
+    double res = sum(omega * T % normal_mgf(x_j, mu, sigma) % P - mu * x_j) +
+	lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2)) +
+	lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) -
+	log(sigma);
+
+    if (verbose)
+	Rcpp::Rcout << "f: " << res << "\n";
+
+    return res;
+}
 
 
-    public:
-	ExponentialVariance(const arma::colvec &P_, const arma::colvec &x_j_, 
-		const arma::colvec &T_, double omega_, double mu_, 
-		double lambda_) :
-	    P(P_), x_j(x_j_), T(T_), omega(omega_), mu(mu_), lambda(lambda_) {};
+double 
+objective_sigma_exp(double sigma, void* args)
+{
+    kwargs *arg = static_cast<kwargs *>(args);
+    double mu = arg->mu;
+    double omega = arg->omega;
+    double lambda = arg->lambda;
+    const arma::vec &P = arg->P;
+    const arma::vec &T = arg->T;
+    const arma::vec x_j = arg->x_j;
+    bool verbose = arg->verbose;
 
-	double Evaluate(const double sigma) {
-	    return sum(omega * T % normal_mgf(x_j, mu, sigma) % P - mu * x_j) +
-		lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2)) +
-		lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) -
-		log(sigma);
-	}
+    double res = sum(omega * T % normal_mgf(x_j, mu, sigma) % P - mu * x_j) +
+	lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2)) +
+	lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) -
+	log(sigma);
 
-	void Gradient(const double sigma, double &g) {
-	    g = sum(omega * T % P % normal_mgf(x_j , mu, sigma) % x_j * sigma) -
-		(1.0 + 2.0 * mu / pow(sigma, 2)) * lambda * sqrt(2.0/PI) *  
-		exp(-pow(mu/sigma, 2)) -
-		2.0 * lambda * mu/pow(sigma, 2)* R::dnorm(- mu / sigma, 0, 1, 0) +
-		1 / sigma;
-	}
-};
+    if (verbose)
+	Rcpp::Rcout << "f: " << res << "\n";
+
+    return res;
+}
+
+
+// [[Rcpp::export]]
+double
+optimise_gamma_exp(double mu, double sigma, double lambda, double omega,
+	double a_0, double b_0, const arma::vec &P, const arma::vec &T, 
+	const arma::vec &x_j, bool verbose) 
+{
+    double res = sigmoid(log(a_0 / b_0) + 1.0/2.0 -
+	    (lambda * sigma * sqrt(2.0 / PI) * exp(-pow(mu/sigma, 2 )) +
+	     lambda * mu * (1.0 - 2.0 * R::pnorm(- mu/sigma, 0, 1, 1, 0)) +
+	     log(sqrt(2.0 / PI) * 1.0 /(sigma * lambda)) +
+	     sum(omega * T % P % (normal_mgf(x_j, mu, sigma) - 1) - mu * x_j)));
+    if (verbose)
+	Rcpp::Rcout << "f: " << res << "\n";
+
+    return res;
+}
 
 
 double
-ExponentialWeights(double mu, double sigma, double lambda, double omega,
-	double a_0, double b_0, const arma::vec &P, const arma::vec &T, 
-	const arma::vec &x_j) 
+optimise_mu_exp(double sigma, double omega, double lambda, 
+    const arma::vec &P, const arma::vec &T, const arma::vec x_j, bool verbose)
 {
-    return sigmoid(log(a_0 / b_0) + 1.0/2.0 -
-	    (lambda * sigma * sqrt(2.0 / PI) * exp(-pow(mu/ sigma, 2 )) +
-	     lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) +
-	     log(sqrt(2.0 / PI) * 1.0 /(sigma * lambda)) +
-	     sum(omega * T % P % (normal_mgf(x_j, mu, sigma) - 1) - mu * x_j)));
+    kwargs args = {0.0, sigma, lambda, omega, P, T, x_j, verbose};
+    return Brent_fmin(-1e2, 1e2, objective_mu_exp, 
+	    static_cast<void*>(&args), 1e-5);
 }
+
+
+double
+optimise_sigma_exp(double mu, double omega, double lambda, 
+    const arma::vec &P, const arma::vec &T, const arma::vec x_j, bool verbose)
+{
+    kwargs args = {mu, 0.0, lambda, omega, P, T, x_j, verbose};
+    return Brent_fmin(0, 4, objective_sigma_exp, 
+	    static_cast<void*>(&args), 1e-5);
+}
+
--- src/exponential.hpp
+++ src/exponential.hpp
@@ -1,108 +1,33 @@
 #ifndef _EXPONENTIAL_HPP
 #define _EXPONENTIAL_HPP
 
+
 #include "RcppArmadillo.h"
 #include "utils.hpp"
+#include "optimiser.hpp"
 
 
-
-struct args {
-    double mu;
-    double sigma;
-    double lambda;
-    double omega;
-    double a_0;
-    double b_0;
-    const arma::vec &P;
-    const arma::vec &T; 
-    const arma::vec &x_j;
-};
+struct args;
 
 
+double 
+objective_mu_exp(double mu, void* args);
 
-class ExponentialMean {
-    private:
-	arma::colvec P;
-	arma::colvec x_j;
-	arma::colvec T;
-	double omega;
-	double sigma;
-	double lambda;
-
-
-    public:
-	ExponentialMean(const arma::colvec &P_, const arma::colvec &x_j_, 
-		const arma::colvec &T_, double omega_, double sigma_, 
-		double lambda_) :
-	    P(P_), x_j(x_j_), T(T_), omega(omega_), sigma(sigma_), 
-	    lambda(lambda_) {};
-
-	double Evaluate(const arma::mat &mu_) {
-	    double mu = mu_(0, 0);
-	    Rcpp::Rcout << "o(mu):" << mu << "\n";
-	    return sum(omega * T % normal_mgf(x_j, mu, sigma) % P - mu * x_j) +
-		lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2)) +
-		lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0));
-	}
-
-	void Gradient(const arma::mat &mu_, arma::mat &g) {
-	    double mu = mu_(0, 0);
-	    g(0, 0) = sum(omega * T % P % normal_mgf(x_j , mu, sigma) % x_j-x_j) -
-		2.0 * lambda * mu/sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2)) +
-		lambda * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) +
-		lambda * 2.0 / sigma * R::dnorm(- mu / sigma, 0, 1, 0);
-	}
-};
-
-
-class ExponentialVariance {
-    private:
-	arma::colvec P;
-	arma::colvec x_j;
-	arma::colvec T;
-	double omega;
-	double mu;
-	double lambda;
-
-
-    public:
-	ExponentialVariance(const arma::colvec &P_, const arma::colvec &x_j_, 
-		const arma::colvec &T_, double omega_, double mu_, 
-		double lambda_) :
-	    P(P_), x_j(x_j_), T(T_), omega(omega_), mu(mu_), lambda(lambda_) {};
-
-	double Evaluate(const arma::mat &sigma_) {
-	    double sigma = sigma_(0, 0);
-	    if (sigma < 0) {
-		return INFINITY;
-	    }
-	    return sum(omega * T % normal_mgf(x_j, mu, sigma) % P - mu * x_j) +
-		lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2)) +
-		lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) -
-		log(sigma);
-	}
-
-	void Gradient(const arma::mat &sigma_, arma::mat &g) {
-	    double sigma = sigma_(0, 0);
-	    g(0, 0) = sum(omega * T % P % normal_mgf(x_j,mu,sigma) % x_j*sigma) -
-		(1.0 + 2.0 * mu / pow(sigma, 2)) * lambda * sqrt(2.0/PI) *  
-		exp(-pow(mu/sigma, 2)) -
-		2.0 * lambda * mu/pow(sigma, 2)* R::dnorm(- mu / sigma, 0, 1, 0) -
-		1 / sigma;
-	}
-};
-
+double 
+objective_sigma_exp(double sigma, void* args);
 
 double
-ExponentialWeights(double mu, double sigma, double lambda, double omega,
+optimise_gamma_exp(double mu, double sigma, double lambda, double omega,
 	double a_0, double b_0, const arma::vec &P, const arma::vec &T, 
-	const arma::vec &x_j) 
-{
-    return sigmoid(log(a_0 / b_0) + 1.0/2.0 -
-	    (lambda * sigma * sqrt(2.0 / PI) * exp(-pow(mu/ sigma, 2 )) +
-	     lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) +
-	     log(sqrt(2.0 / PI) * 1.0 /(sigma * lambda)) +
-	     sum(omega * T % P % (normal_mgf(x_j, mu, sigma) - 1) - mu * x_j)));
-}
+	const arma::vec &x_j, bool verbose);
+
+double
+optimise_mu_exp(double sigma, double omega, double lambda, 
+    const arma::vec &P, const arma::vec &T, const arma::vec x_j, bool verbose);
+
+double
+optimise_sigma_exp(double mu, double omega, double lambda, 
+    const arma::vec &P, const arma::vec &T, const arma::vec x_j, bool verbose);
+
 
 #endif
