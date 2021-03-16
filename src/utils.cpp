5e4bf21a509699316afba9d878a3217b1f5ac87e
mkomod <mkomodromos01@gmail.com>
2021-03-16 21:59:45 +0200

    fix: header issue

---
+++ src/utils.cpp
@@ -0,0 +1,20 @@
+#include "utils.hpp"
+
+
+double
+normal_mgf(double x, double m, double s)
+{
+    return exp(x * m + 1.0/2.0 * x * x * s * s);
+}
+
+arma::vec
+normal_mgf(arma::vec x, double m, double s)
+{
+    return exp(x * m + 1.0/2.0 * pow(x * s, 2));
+}
+
+double 
+sigmoid(double x)
+{
+    return 1 / (1 + exp(-x));
+}
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,24 +1,10 @@
-#ifndef _UTILS_H
-#define _UTILS_H
+#ifndef UTILS_HPP
+#define UTILS_HPP
 
 #include "RcppArmadillo.h"
 
-double
-normal_mgf(double x, double m, double s)
-{
-    return exp(x * m + 1.0/2.0 * x * x * s * s);
-}
-
-arma::vec
-normal_mgf(arma::vec x, double m, double s)
-{
-    return exp(x * m + 1.0/2.0 * pow(x * s, 2));
-}
-
-double sigmoid(double x)
-{
-    return 1 / (1 + exp(-x));
-}
-
+double normal_mgf(double x, double m, double s);
+arma::vec normal_mgf(arma::vec x, double m, double s);
+double sigmoid(double x);
 
 #endif
