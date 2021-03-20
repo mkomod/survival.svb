a2dbbc8ab370c7f2f48d7c3b52919abfc6a77119
mkomod <mkomodromos01@gmail.com>
2021-03-20 13:51:09 +0200

    rf: refactor, rm unused code

--- R/comp.R
+++ R/comp.R
@@ -1,57 +1,9 @@
 # Comparisons
 mse <- function(x) mean(x^2)
 
-
-# test without censoring
+# simulations
 for (n in c(50, 100, 250, 1000)) {
-    set.seed(1)
-    b <- c(1, 1, rep(0, 50))
-    p <- length(b)
-    X <- matrix(rnorm(n * p), nrow=n)
-    y <- runif(nrow(X))
-    omega <- 1
-    Y <- log(1 - y) / - (exp(X %*% b) * omega)
-    delta  <- rep(1, n)
-
-    m <- matrix(rnorm(p), ncol=1)
-    s <- matrix(abs(rnorm(p)), ncol=1)
-    g <- matrix(runif(p), ncol=1)
-
-    lambda <- 4
-    res <- fit(Y, delta, X, omega, lambda, 1, 1, m, s, g, 1000, F)
-    m.1 <- survival::coxph(survival::Surv(Y) ~ ., data = as.data.frame(X))
-
-    cat("Sparse Surv: ", mse(res$m - b), "\n",
-	"CoxPH: ", mse(coef(m.1) - b), "\n")
-}
-
-plot(b, col="grey", pch=20, ylim=c(-0.2, 1.2), ylab=expression(beta))
-points(res$m, col="red", pch=20)
-
-# Sparse Surv:  0.0073
-# CoxPH:  (NA) 7232.961
-# Sparse Surv:  0.0035
-# CoxPH:  0.0894
-# Sparse Surv:  0.0029
-# CoxPH:  0.0109
-# Sparse Surv:  0.00088
-# CoxPH:  0.00135
-
-microbenchmark::microbenchmark(
-    fit(Y, delta, X, omega, lambda, 1, 1, m, s, g, 1000, F),
-    survival::coxph(survival::Surv(Y) ~ ., data = as.data.frame(X)),
-    times=10
-)
-
-# Unit: millisecondsn asymptoti
-#        min        lq      mean    median        uq        max   neval cld
-# svb   602.2979 620.6298 741.68978 640.76466 808.35299  1513.036  100   b
-# coxph 45.80873  63.3845  70.65804  67.79749  74.95692   139.988  100   a
-
-
-# test with censoring
-for (n in c(50, 100, 250, 1000)) {
-    for (censoring_lvl in c(0.10, 0.25, 0.5, 0.75)) {
+    for (censoring_lvl in c(0.00, 0.10, 0.25, 0.5, 0.75)) {
 	set.seed(1)
 	b <- c(1, 1, rep(0, 50))
 	p <- length(b)
@@ -59,7 +11,7 @@ for (n in c(50, 100, 250, 1000)) {
 	y <- runif(nrow(X))
 	omega <- 1
 	Y <- log(1 - y) / - (exp(X %*% b) * omega)
-	delta  <- runif(n) < censoring_lvl   # 0: censored, 1: uncensored
+	delta  <- runif(n) > censoring_lvl   # 0: censored, 1: uncensored
 	Y[!delta] <- Y[!delta] * runif(sum(!delta))
 
 	# params
@@ -67,14 +19,24 @@ for (n in c(50, 100, 250, 1000)) {
 	s <- matrix(abs(rnorm(p)), ncol=1)
 	g <- matrix(runif(p), ncol=1)
 	lambda <- 4
-	res <- fit(Y, delta, X, omega, lambda, 1, 1, m, s, g, 1000, F)
-	m.1 <- survival::coxph(survival::Surv(Y, delta) ~ ., 
-			       data = as.data.frame(X))
-	cat("Sparse Surv: ", mse(res$m - b), "\n",
-	"CoxPH: ", mse(coef(m.1) - b), "\n")
-
+	res <- fit(Y, delta, X, lambda, 1, 1, 1e-3, 1e-3, m, s, g, 1000, F)
+	# m.1 <- survival::coxph(survival::Surv(Y, delta) ~ ., 
+	# 		       data = as.data.frame(X))
+	cat("n:", n, 
+	    sprintf("\tc: %.3f", censoring_lvl), 
+	    sprintf("\tmse: %.5f", mse(res$m - b)), "\n")
     }
 }
 
 
+microbenchmark::microbenchmark(
+    fit(Y, delta, X, lambda, 1, 1, 1e-3, 1e-3, m, s, g, 1000, F),
+    survival::coxph(survival::Surv(Y) ~ ., data = as.data.frame(X)),
+    times=10
+)
+
+# ms
+#     	   mean  median 
+# svb   7336.12 7357.00 
+# coxph   68.59   67.72 
 
