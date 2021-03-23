1a5c78da91c0b999085681ebe6641dc45d92a5fd
mkomod <mkomodromos01@gmail.com>
2021-03-23 16:14:59 +0200

    mv: init_P, add_P, rm_P to utils

--- src/utils.cpp
+++ src/utils.cpp
@@ -18,3 +18,30 @@ sigmoid(double x)
 {
     return 1 / (1 + exp(-x));
 }
+
+arma::vec
+init_P(arma::mat X, arma::vec m, arma::vec s, arma::vec g)
+{
+    int n = X.n_rows;
+    int p = X.n_cols;
+    arma::vec P(n, arma::fill::zeros);
+    for (int i = 0; i < n; ++i) {
+	for (int j = 0; j < p; ++j) {
+	    // Prod x = exp(log(Prod x)) = exp(Sum log x))
+	    P(i) += log(g(j) * normal_mgf(X(i, j), m(j), s(j)) + 1.0-g(j));
+	}
+    }
+    return exp(P);
+}
+
+arma::vec
+rm_P(arma::vec P, arma::vec x_j, double m, double s, double g)
+{
+    return P /= g * normal_mgf(x_j, m, s) + (1 - g);
+}
+
+arma::vec
+add_P(arma::vec P, arma::vec x_j, double m, double s, double g)
+{
+    return P %= g * normal_mgf(x_j, m, s) + (1 - g);
+}
--- src/utils.hpp
+++ src/utils.hpp
@@ -3,9 +3,13 @@
 
 #include "RcppArmadillo.h"
 
+double sigmoid(double x);
+
 double normal_mgf(double x, double m, double s);
 arma::vec normal_mgf(arma::vec x, double m, double s);
 
-double sigmoid(double x);
+arma::vec init_P(arma::mat X, arma::vec m, arma::vec s, arma::vec g);
+arma::vec rm_P(arma::vec P, arma::vec x_j, double m, double s, double g);
+arma::vec add_P(arma::vec P, arma::vec x_j, double m, double s, double g);
 
 #endif
