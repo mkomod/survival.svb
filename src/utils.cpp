53241ea62a2bef9d5a769a48e66ccca32b2fd01b
mkomod <mkomodromos01@gmail.com>
2021-06-09 22:09:24 +0100

    rf: cleanup and comments

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,73 +1,44 @@
 #include "utils.hpp"
+#include <vector>
 
-
-double
-normal_mgf(double x, double m, double s)
+double normal_mgf(double x, double m, double s)
 {
     return exp(x * m + 1.0/2.0 * x * x * s * s);
 }
 
-arma::vec
-normal_mgf(const arma::vec &x, double m, double s)
+arma::vec normal_mgf(const arma::vec &x, double m, double s)
 {
     return exp(x * m + 1.0/2.0 * pow(x * s, 2));
 }
 
 
-arma::vec
-normal_mgf(const arma::vec &x, const arma::vec &m, const arma::vec &s) 
+arma::vec normal_mgf(const arma::vec &x, const arma::vec &m, 
+    const arma::vec &s)  
 {
     return exp(x % m + 0.5 * pow(x % s, 2));
 }
 
 
-arma::vec
-log_normal_mgf(const arma::vec &x, double m, double s)
+arma::vec log_normal_mgf(const arma::vec &x, double m, double s) 
 {
     return x * m + 1.0/2.0 * pow(x * s, 2);
 }
 
 
-double 
-sigmoid(double x)
+double sigmoid(double x) 
 {
     return 1 / (1 + exp(-x));
 }
 
 
-double
-P_ij(double x_ij,  double m, double s, double g) 
-{
-    // Computer the log P(x_ij) = log(1 - g + g * normal_mgf(x_ij, mu, sig))
-    // log(1 - g + g * exp(M)) == x + log(g + (1-g)*exp(-x))
-    // the second expression is used when x is large ( > 0 ) to prevent overflow
-    if (g == 0.0) {
-	return 0;
-    } 
-
-    double x = x_ij * m + 0.5 * x_ij * x_ij * s * s;
-    if (g == 1.0) {
-	return x;
-    }
-
-    double res = 0.0;
-    if (x > 0) {
-	res = x + log(g + (1-g)*exp(-x));
-    } else {
-	res = log(1-g + g*exp(x));
-    }
-    return res;
-}
-
-arma::vec
-init_P(const arma::mat &X, const arma::vec &m, const arma::vec &s, 
+arma::vec init_P(const arma::mat &X, const arma::vec &m, const arma::vec &s, 
 	const arma::vec &g)
 {
-    int n = X.n_rows;
-    int p = X.n_cols;
+    arma::uword n = X.n_rows;
+    arma::uword p = X.n_cols;
     arma::vec P(n, arma::fill::zeros);
-    for (int i = 0; i < n; ++i) {
-	for (int j = 0; j < p; ++j) {
+    for (arma::uword i = 0; i < n; ++i) {
+	for (arma::uword j = 0; j < p; ++j) {
 	    // Prod x = exp(log(Prod x)) = exp(Sum log x))
 	    P(i) += log(g(j) * normal_mgf(X(i, j), m(j), s(j)) + 1.0-g(j));
 	}
@@ -75,30 +46,46 @@ init_P(const arma::mat &X, const arma::vec &m, const arma::vec &s,
     return exp(P);
 }
 
-arma::vec
-rm_P(arma::vec P, const arma::vec &x_j, double m, double s, double g)
+
+arma::vec rm_P(arma::vec P, const arma::vec &x_j, double m, double s, double g)
 {
     return P /= (g * normal_mgf(x_j, m, s) + (1 - g));
 }
 
-arma::vec
-add_P(arma::vec P, const arma::vec &x_j, double m, double s, double g)
+
+arma::vec add_P(arma::vec P, const arma::vec &x_j, double m, double s, double g)
 {
     return P %= (g * normal_mgf(x_j, m, s) + (1 - g));
 }
 
 
-arma::vec
-init_log_P(const arma::mat &X, const arma::vec &m, const arma::vec &s, 
+// Compute the log P(x_ij) = log(1 - g + g * normal_mgf(x_ij, mu, sig))
+// log(1 - g + g * exp(x)) = x + log(g + (1-g)*exp(-x))
+//           ^--- multiply by exp(x)/exp(x)
+static double P_ij(double x_ij,  double m, double s, double g) 
+{
+    if (g == 0.0)
+	return 0;
+    
+    double x = x_ij * m + 0.5 * x_ij * x_ij * s * s; // log(normal mgf)
+    if (g == 1.0)
+	return x;
+
+    return x > 0 ? x + log(g + (1-g)*exp(-x)) : log(1-g + g*exp(x));
+}
+
+
+// [[Rcpp::export]]
+arma::vec init_log_P(const arma::mat &X, const arma::vec &m, const arma::vec &s,
 	const arma::vec &g)
 {
-    int n = X.n_rows;
-    int p = X.n_cols;
+    arma::uword n = X.n_rows;
+    arma::uword p = X.n_cols;
     arma::vec P(n, arma::fill::zeros);
     double x = 0.0;
 
-    for (int i = 0; i < n; ++i) {
-	for (int j = 0; j < p; ++j)  {
+    for (arma::uword i = 0; i < n; ++i) {
+	for (arma::uword j = 0; j < p; ++j)  {
 	    P(i) += P_ij(X(i, j), m(j), s(j), g(j));
 	}
     }
@@ -106,18 +93,18 @@ init_log_P(const arma::mat &X, const arma::vec &m, const arma::vec &s,
     return P;
 }
 
-void
-rm_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g)
+
+void rm_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g)
 {
-    for (int i = 0; i < x_j.n_rows; ++i)  {
+    for (arma::uword i = 0; i < x_j.n_rows; ++i)  {
 	P(i) -= P_ij(x_j(i), m, s, g);
     }
 }
 
-void
-add_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g)
+
+void add_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g)
 {
-    for (int i = 0; i < x_j.n_rows; ++i)  {
+    for (arma::uword i = 0; i < x_j.n_rows; ++i)  {
 	P(i) += P_ij(x_j(i), m, s, g);
     }
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -5,21 +5,23 @@
 
 double sigmoid(double x);
 
+// normal funcs
 double normal_mgf(double x, double m, double s);
 arma::vec normal_mgf(const arma::vec &x, double m, double s);
 arma::vec normal_mgf(const arma::vec &x, const arma::vec &m, const arma::vec &s);
 arma::vec log_normal_mgf(const arma::vec &x, double m, double s);
 
+// P
 arma::vec init_P(const arma::mat &X, const arma::vec &m, const arma::vec &s, 
-	const arma::vec &g);
-arma::vec rm_P(arma::vec P, const arma::vec &x_j, double m, double s,
-	double g);
-arma::vec add_P(arma::vec P, const arma::vec &x_j, double m, double s, 
-	double g);
+    const arma::vec &g);
+arma::vec rm_P(arma::vec P, const arma::vec &x_j, double m, double s, double g);
+arma::vec add_P(arma::vec P, const arma::vec &x_j, double m, double s, double g);
 
+// log P
 arma::vec init_log_P(const arma::mat &X, const arma::vec &m, const arma::vec &s, 
-	const arma::vec &g);
+    const arma::vec &g);
 void rm_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g);
 void add_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g);
+
 
 #endif
