f30cbd0b472214d0c6e1659e60e7d595ea54f1a6
mkomod <mkomodromos01@gmail.com>
2021-06-09 21:59:52 +0100

    rm: license, instead add where license could be found

--- src/optimiser.cpp
+++ src/optimiser.cpp
@@ -1,29 +1,7 @@
-/*
- *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- *  Copyright (C) 2003-2004  The R Foundation
- *  Copyright (C) 1998--2014  The R Core Team
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, a copy is available at
- *  https://www.R-project.org/Licenses/
- *
- *
- *  See: R/src/library/stats/src/optimize.c
- */
 #include "optimiser.hpp"
 
 
+// Copied from R/src/library/stats/src/optimize.c
 double Brent_fmin(double ax, double bx, double (*f)(double, void *),
 		  void *info, double tol)
 {
