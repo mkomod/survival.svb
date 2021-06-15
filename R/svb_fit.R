3c6ae1206d943b9bc8cc402b61017d828cbf2512
mkomod <mkomodromos01@gmail.com>
2021-06-15 12:43:17 +0100

    up: docs

--- R/svb_fit.R
+++ R/svb_fit.R
@@ -1,17 +1,23 @@
 #' Fit sparse variational Bayesian survival models.
-#' 
-#' @param Y observation times
-#' @param delta Inidcator for right censoring 0: censored, 1: uncensored
-#' @param X model matrix
-#' @param lambda penalisation term
-#' @param params model parameters partial: c(a_0, b_0), exponential: c(a_0, b_0, a_omega, b_omega).
-#' @param mu.init initial values for means
-#' @param s.init initial values for standard deviations
-#' @param g.init initial values for gamma, the inclusion probability
-#' @param model one of "partial", "exponential"
+#'
+#' @param Y failure times
+#' @param delta censoring indicator, 0: censored, 1: uncensored
+#' @param X design matrix
+#' @param lambda penalisation hyperparameter
+#' @param model one of: \itemize{
+#' 	\item{\code{"partial"}} (defualt),
+#'	\item{\code{"exponential"}}
+#' }
+#' @param params additional hyperparameters, defualts: \itemize{
+#' 	\item{partial}: \code{c(a_0=1, b_0=ncol(X))},
+#' 	\item{exponential}: \code{c(a_0=1, b_0=ncol(X), a_omega=1, b_omega=1)}.
+#' }
+#' @param mu.init initial values for means, default: \code{rnorm(ncol(X))}
+#' @param s.init initial values for standard deviations, default: \code{rep(0.2, ncol(X))}
+#' @param g.init initial values for gamma, default: \code{rep(0.5, ncol(X))}
 #' @param maxiter maximum number of iterations
-#' @param tol Stopping tolerance
-#' @param verbose print information
+#' @param tol convergence tolerance
+#' @param verbose print additional information
 #'
 #' @examples
 #' n <- 200
@@ -29,11 +35,11 @@
 #' Y[!delta] <- Y[!delta] * runif(sum(!delta))
 #' 
 #' lambda <- 0.5
-#' svb.fit(Y, delta, X)
+#' f <- svb.fit(Y, delta, X)
 #'
 #' @export
-svb.fit <- function(Y, delta, X, lambda=0.5, params=NULL, mu.init=NULL, s.init=NULL, 
-    g.init=NULL, model="partial", maxiter=1e3, tol=1e-3, verbose=TRUE)
+svb.fit <- function(Y, delta, X, lambda=0.5, model="partial", params=NULL, 
+    mu.init=NULL, s.init=NULL, g.init=NULL, maxiter=1e3, tol=1e-3, verbose=TRUE)
 {
 
     if (!(model %in% c("partial", "exponential"))) stop("'model' unknown")
