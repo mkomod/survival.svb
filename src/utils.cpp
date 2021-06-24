7fd52274012bd0e9741854e65d3b01a834e7cc6c
mkomod <mkomodromos01@gmail.com>
2021-06-24 19:21:34 +0100

    ft: use eigen, major tidy up, add: some funcs, rf: cleanup

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,61 +1,33 @@
 #include "utils.hpp"
 
 
+// [[Rcpp::export]]
 double normal_mgf(double x, double m, double s)
 {
-    return exp(x * m + 1.0/2.0 * x * x * s * s);
+    return exp(x * m + 0.5 * x * x * s * s);
 }
 
-arma::vec normal_mgf(const arma::vec &x, double m, double s)
+
+double log_normal_mgf(double x, double m, double s) 
 {
-    return exp(x * m + 1.0/2.0 * pow(x * s, 2));
+    return m * x + 0.5 * s * s * x * x;
 }
 
 
-arma::vec normal_mgf(const arma::vec &x, const arma::vec &m, 
-    const arma::vec &s)  
+// [[Rcpp::export]]
+vec log_normal_mgf(const vec &x, double m, double s) 
 {
-    return exp(x % m + 0.5 * pow(x % s, 2));
+    vec res = vec(x.rows());
+    for (int i = 0; i < x.rows(); ++i) {
+	res(i) = m * x(i) + 0.5 * s * s * x(i) * x(i);
+    }
+    return res;
 }
 
-
-arma::vec log_normal_mgf(const arma::vec &x, double m, double s) 
-{
-    return x * m + 1.0/2.0 * pow(x * s, 2);
-}
-
-
+// [[Rcpp::export]]
 double sigmoid(double x) 
 {
     return 1 / (1 + exp(-x));
-}
-
-
-arma::vec init_P(const arma::mat &X, const arma::vec &m, const arma::vec &s, 
-	const arma::vec &g)
-{
-    arma::uword n = X.n_rows;
-    arma::uword p = X.n_cols;
-    arma::vec P(n, arma::fill::zeros);
-    for (arma::uword i = 0; i < n; ++i) {
-	for (arma::uword j = 0; j < p; ++j) {
-	    // Prod x = exp(log(Prod x)) = exp(Sum log x))
-	    P(i) += log(g(j) * normal_mgf(X(i, j), m(j), s(j)) + 1.0-g(j));
-	}
-    }
-    return exp(P);
-}
-
-
-arma::vec rm_P(arma::vec P, const arma::vec &x_j, double m, double s, double g)
-{
-    return P /= (g * normal_mgf(x_j, m, s) + (1 - g));
-}
-
-
-arma::vec add_P(arma::vec P, const arma::vec &x_j, double m, double s, double g)
-{
-    return P %= (g * normal_mgf(x_j, m, s) + (1 - g));
 }
 
 
@@ -70,21 +42,26 @@ static double P_ij(double x_ij,  double m, double s, double g)
     double x = x_ij * m + 0.5 * x_ij * x_ij * s * s; // log(normal mgf)
     if (g == 1.0)
 	return x;
-
-    return x > 0 ? x + log(g + (1-g)*exp(-x)) : log(1-g + g*exp(x));
+    
+    if (x > 0) {
+	return x + log(g + (1-g)*exp(-x));
+    } else {
+	return log(1-g + g*exp(x));
+    }
 }
 
 
-arma::vec init_log_P(const arma::mat &X, const arma::vec &m, const arma::vec &s,
-	const arma::vec &g)
+// [[Rcpp::export]]
+vec init_log_P(const mat &X, const vec &m, const vec &s, const vec &g)
 {
-    arma::uword n = X.n_rows;
-    arma::uword p = X.n_cols;
-    arma::vec P(n, arma::fill::zeros);
+    uint n = X.rows();
+    uint p = X.cols();
+    vec P = vec::Zero(n);
+
     double x = 0.0;
 
-    for (arma::uword i = 0; i < n; ++i) {
-	for (arma::uword j = 0; j < p; ++j)  {
+    for (uint i = 0; i < n; ++i) {
+	for (uint j = 0; j < p; ++j)  {
 	    P(i) += P_ij(X(i, j), m(j), s(j), g(j));
 	}
     }
@@ -93,19 +70,79 @@ arma::vec init_log_P(const arma::mat &X, const arma::vec &m, const arma::vec &s,
 }
 
 
-void rm_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g)
+// untested
+void rm_log_P(vec &P, const vec &x_j, double m, double s, double g)
 {
-    for (arma::uword i = 0; i < x_j.n_rows; ++i)  {
+    for (uint i = 0; i < x_j.rows(); ++i)  {
 	P(i) -= P_ij(x_j(i), m, s, g);
     }
 }
 
 
-void add_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g)
+// untested
+void add_log_P(vec &P, const vec &x_j, double m, double s, double g)
 {
-    for (arma::uword i = 0; i < x_j.n_rows; ++i)  {
+    for (uint i = 0; i < x_j.rows(); ++i)  {
 	P(i) += P_ij(x_j(i), m, s, g);
     }
 }
 
+
+// order the failure times
+// [[Rcpp::export]]
+std::vector<uint> order_T(const vec &T)
+{
+    // order(T)
+    std::vector<uint> T_ord(T.size());
+    std::iota(T_ord.begin(), T_ord.end(), 0);
+
+    std::stable_sort(T_ord.begin(), T_ord.end(),
+	    [&T](uint i1, uint i2) {return T[i1] < T[i2];});
+ 
+    return T_ord;
+}
+
+
+// get the index where failures occured for sorted T (times)
+// [[Rcpp::export]]
+std::vector<uint> order_delta(const vec &T, const vec &delta) 
+{
+    std::vector<uint> T_ord = order_T(T);
+    std::vector<uint> delta_ord;
+
+    for (uint i = 0; i < T_ord.size(); ++i)
+	if (delta(T_ord.at(i)) == 1) delta_ord.push_back(i);
+
+    return delta_ord;
+}
+
+
+// compute the log log_likelihood
+// [[Rcpp::export]]
+double log_likelihood(const vec &b, const mat &X, const vec &T, const vec &delta)
+{
+    std::vector<uint> T_ord = order_T(T);
+    std::vector<uint> delta_ord = order_delta(T, delta);
+
+    vec xb = X * b;
+    double a = xb.maxCoeff();		// prevent overflow
+    double den = 0.0;
+    double tot = 0.0;
+
+    uint icf = 0;			// index of current failure
+    uint ilf = X.rows();		// index of last failure
+
+    for (int i = delta_ord.size() - 1; i >= 0; --i) {
+	icf = delta_ord.at(i);		// set the current failure to i
+
+	for (uint j = icf; j <= ilf - 1; ++j)
+	    den += exp(xb(T_ord.at(j)) - a);		// prevent overflow
+	
+	tot += xb(T_ord.at(icf)) - (a + log(den));
+
+	ilf = icf;			// set the index of last failure to current
+    }
+
+    return tot;
+}
 
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,27 +1,24 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#include "RcppArmadillo.h"
+#include "RcppEigen.h"
+#include "survival.svb_types.h"
 
-double sigmoid(double x);
-
-// normal funcs
+// normal mgf funcs
 double normal_mgf(double x, double m, double s);
-arma::vec normal_mgf(const arma::vec &x, double m, double s);
-arma::vec normal_mgf(const arma::vec &x, const arma::vec &m, const arma::vec &s);
-arma::vec log_normal_mgf(const arma::vec &x, double m, double s);
-
-// P
-arma::vec init_P(const arma::mat &X, const arma::vec &m, const arma::vec &s, 
-    const arma::vec &g);
-arma::vec rm_P(arma::vec P, const arma::vec &x_j, double m, double s, double g);
-arma::vec add_P(arma::vec P, const arma::vec &x_j, double m, double s, double g);
+vec log_normal_mgf(const vec &x, double m, double s);
+double log_normal_mgf(double x, double m, double s);
 
 // log P
-arma::vec init_log_P(const arma::mat &X, const arma::vec &m, const arma::vec &s, 
-    const arma::vec &g);
-void rm_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g);
-void add_log_P(arma::vec &P, const arma::vec &x_j, double m, double s, double g);
+vec init_log_P(const mat &X, const vec &m, const vec &s, const vec &g);
+void rm_log_P(vec &P, const vec &x_j, double m, double s, double g);
+void add_log_P(vec &P, const vec &x_j, double m, double s, double g);
+
+// misc
+double sigmoid(double x);
+std::vector<uint> order_T(const vec &T);
+std::vector<uint> order_delta(const vec &T, const vec &delta);
+double log_likelihood(const vec &b, const mat &X, const vec &T, const vec &delta);
 
 
 #endif
