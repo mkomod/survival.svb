c3a968aa956b91e385b6750ec292b7cb7407a679
mkomod <mkomodromos01@gmail.com>
2021-06-24 19:20:55 +0100

    use: eigen

--- src/partial.cpp
+++ src/partial.cpp
@@ -1,41 +1,50 @@
 #include "partial.hpp"
 
-#include <math.h>
-#include <vector>
-
-#include "utils.hpp"
-#include "optimiser.hpp"
 
 struct kwargs {
     double mu; 
     double sigma; 
     double lambda; 
-    const std::vector<arma::uvec> &R;
-    const arma::uvec &F;
-    const arma::vec &P;
-    const arma::vec &x_j;
+    const vec &P;
+    const vec &x_j;
+    const std::vector<uint> &delta_ord;
 };
 
 
-static double  f_par_mu(double mu, void* args)
+static double pm(double mu, double sigma, const vec &P, const vec &x_j, 
+	const std::vector<uint> &delta_ord) 
+{
+    double s = 0.0;
+    double t = 0.0;
+    double a = (P + log_normal_mgf(x_j, mu, sigma)).maxCoeff();
+	
+    uint icf = 0;
+    uint ilf = x_j.rows();
+
+    for (int i = delta_ord.size() - 1; i >= 0; --i) {
+	icf = delta_ord.at(i);
+
+	for (uint j = icf; j <= ilf - 1; ++j)
+	    s += exp(P(j) + log_normal_mgf(x_j(j), mu, sigma) - a);
+	
+	t += a + log(s) - mu * x_j(icf);
+
+	ilf = icf;
+    }
+
+    return t;
+}
+
+static double f_par_mu(double mu, void* args)
 {
     kwargs *arg = static_cast<kwargs *>(args);
     double sigma = arg->sigma;
     double lambda = arg->lambda;
-    const std::vector<arma::uvec> &R = arg->R;
-    const arma::uvec &F = arg->F;
-    const arma::vec &P = arg->P;
-    const arma::vec &x_j = arg->x_j;
+    const vec &P = arg->P;
+    const vec &x_j = arg->x_j;
+    const std::vector<uint> &delta_ord = arg->delta_ord;
 
-    double t = 0.0;
-    double s = 0.0;
-    double a = max(P + log_normal_mgf(x_j, mu, sigma));
-    for (auto it = R.rbegin(); it != R.rend(); ++it) {
-	arma::uvec risk_set = (*it);
-	s += sum(exp(P(risk_set) + log_normal_mgf(x_j(risk_set), mu, sigma) - a));
-	t += a + log(s);
-    }
-    t -= mu * sum(x_j(F));
+    double t = pm(mu, sigma, P, x_j, delta_ord);
 
     double res = t +
 	lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2) * 0.5) +
@@ -51,20 +60,11 @@ static double f_par_sig(double sigma, void* args)
     kwargs *arg = static_cast<kwargs *>(args);
     double mu = arg->mu;
     double lambda = arg->lambda;
-    const std::vector<arma::uvec> &R = arg->R;
-    const arma::uvec &F = arg->F;
-    const arma::vec &P = arg->P;
-    const arma::vec &x_j = arg->x_j;
-
-    double t = 0.0;
-    double s = 0.0;
-    double a = max(P + log_normal_mgf(x_j, mu, sigma));
-    for (auto it = R.rbegin(); it != R.rend(); ++it) {
-	arma::uvec risk_set = (*it);
-	s += sum(exp(P(risk_set) + log_normal_mgf(x_j(risk_set), mu, sigma) - a));
-	t += a + log(s);
-    }
-    t -= mu * sum(x_j(F));
+    const vec &P = arg->P;
+    const vec &x_j = arg->x_j;
+    const std::vector<uint> &delta_ord = arg->delta_ord;
+    
+    double t = pm(mu, sigma, P, x_j, delta_ord);
 
     double res = t +
 	lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2) * 0.5) +
@@ -75,45 +75,59 @@ static double f_par_sig(double sigma, void* args)
 }
 
 
-double opt_par_mu(double sigma, double lambda, const std::vector<arma::uvec> &R,
-    const arma::uvec &F, const arma::vec &P, const arma::vec &x_j)
+// [[Rcpp::export]]
+double opt_par_mu(double sigma, double lambda, const vec &P, const vec &x_j,
+	const std::vector<uint> &delta_ord)
 {
-    kwargs args = { 0.0, sigma, lambda, R, F, P, x_j };
+    kwargs args = { 0.0, sigma, lambda, P, x_j, delta_ord };
     double res = Brent_fmin(-1e2, 1e2, f_par_mu, static_cast<void *>(&args), 1e-5);
     return res;
 }
 
 
-double opt_par_sig(double mu, double lambda, const std::vector<arma::uvec> &R,
-    const arma::uvec &F, const arma::vec &P, const arma::vec &x_j)
+// [[Rcpp::export]]
+double opt_par_sig(double mu, double lambda, const vec &P, const vec &x_j,
+	const std::vector<uint> &delta_ord)
 {
-    kwargs args = { mu, 0.0, lambda, R, F, P, x_j };
+    kwargs args = { mu, 0.0, lambda, P, x_j, delta_ord };
     double res = Brent_fmin(0.0, 10.0, f_par_sig, static_cast<void *>(&args), 1e-5);
     return res;
 }
 
 
-double opt_par_gam(double mu, double sigma, double a_0, double b_0, 
-    double lambda, const std::vector<arma::uvec> &R, const arma::uvec &F,
-    const arma::vec &P, const arma::vec &x_j)
+// [[Rcpp::export]]
+double opt_par_gam(double mu, double sigma, double lambda, double a_0, double b_0, 
+    const vec &P, const vec &x_j, const std::vector<uint> &delta_ord)
 {
-    double t = 0.0; double s = 0.0; double p = 0.0; double a = 0.0; double b = 0.0;
+    double t = 0.0; 
+    double s = 0.0; 
+    double p = 0.0; 
+    double a = 0.0; 
+    double b = 0.0;
 
-    a = std::min(max(P + log_normal_mgf(x_j, mu, sigma)) / 5.0, 175.0);
-    b = std::min(max(P) / 5.0, 175.0);
+    a = std::min((P + log_normal_mgf(x_j, mu, sigma)).maxCoeff() / 5.0, 175.0);
+    b = std::min(P.maxCoeff() / 5.0, 175.0);
+    uint icf = 0;
+    uint ilf = x_j.rows();
 
-    for (auto it = R.rbegin(); it != R.rend(); ++it) {
-	arma::uvec risk_set = (*it);
-	s += sum(exp(P(risk_set) + log_normal_mgf(x_j(risk_set), mu, sigma) - a));
-	p += sum(exp(P(risk_set) - b));
+    for (int i = delta_ord.size() - 1; i >= 0; --i) {
+	icf = delta_ord.at(i);
+
+	for (uint j = icf; j <= ilf - 1; ++j) {
+	    s += exp(P(j) + log_normal_mgf(x_j(j), mu, sigma) - a);
+	    p += exp(P(j) - b);
+	}
+	
 	t += (a + log(s)) - (b  + log(p));
+
+	ilf = icf;
     }
 
-    double res = sigmoid(
-	    log(a_0 / b_0) + 0.5 -(
-	     lambda * sigma * sqrt(2.0 / PI) * exp(-pow(mu/sigma, 2.0) * 0.5) +
-	     lambda * mu * (1.0 - 2.0 * R::pnorm(- mu/sigma, 0, 1, 1, 0)) +
-	     log(sqrt(2.0 / PI) * 1.0 /(sigma * lambda)) + t));
+    double res = sigmoid(log(a_0 / b_0) + 0.5 - (
+	lambda * sigma * sqrt(2.0 / PI) * exp(-pow(mu/sigma, 2.0) * 0.5) +
+	lambda * mu * (1.0 - 2.0 * R::pnorm(- mu/sigma, 0, 1, 1, 0)) +
+	log(sqrt(2.0 / PI) * 1.0 /(sigma * lambda)) + t)
+    );
     
     return res;
 }
--- src/partial.hpp
+++ src/partial.hpp
@@ -1,20 +1,22 @@
 #ifndef PARTIAL_HPP
 #define PARTIAL_HPP
 
-#include "RcppArmadillo.h"
+#include <math.h>
+#include <vector>
+
+#include "RcppEigen.h"
+#include "optimiser.hpp"
+#include "utils.hpp"
+#include "survival.svb_types.h"
 
 
-double opt_par_mu(double sigma, double lambda, 
-    const std::vector<arma::uvec> &R, const arma::uvec &F, 
-    const arma::vec &P, const arma::vec &x_j);
+double opt_par_mu(double sigma, double lambda, const vec &P, const vec &x_j,
+    const std::vector<uint> &order_delta);
 
-double opt_par_sig(double mu, double lambda, 
-    const std::vector<arma::uvec> &R, const arma::uvec &F, 
-    const arma::vec &P, const arma::vec &x_j);
+double opt_par_sig(double mu, double lambda, const vec &P, const vec &x_j,
+    const std::vector<uint> &order_delta);
 
-double opt_par_gam(double mu, double sigma, double a_0, double b_0, 
-    double lambda, const std::vector<arma::uvec> &R, const arma::uvec &F, 
-    const arma::vec &P, const arma::vec &x_j);
-
+double opt_par_gam(double mu, double sigma, double lambda, double a_0, double b_0,
+    const vec &P, const vec &x_j, const std::vector<uint> &order_delta);
 
 #endif
