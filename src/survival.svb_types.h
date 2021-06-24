494eb9c86d84c430947aa06d886408bd45992a2c
mkomod <mkomodromos01@gmail.com>
2021-06-24 19:22:32 +0100

    ft: needed for Rcpp typedefs

---
+++ src/survival.svb_types.h
@@ -0,0 +1,11 @@
+#ifndef SURVIVAL_SVB_HPP
+#define SURVIVAL_SVB_HPP
+
+#include "RcppEigen.h"
+
+typedef Eigen::VectorXd vec;
+typedef Eigen::VectorXi veci;
+typedef Eigen::MatrixXd mat;
+typedef Eigen::MatrixXi mati;
+
+#endif
