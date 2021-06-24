ebf330f75bbf113ea33e3b6d7f2fc7a1e812bb7c
mkomod <mkomodromos01@gmail.com>
2021-06-24 19:21:13 +0100

    ft: use eigen

--- src/survival_svb.cpp
+++ src/survival_svb.cpp
@@ -1,93 +1,25 @@
 #include <math.h>
 #include <vector>
 
-#include "RcppArmadillo.h"
+#include "RcppEigen.h"
 
-#include "exponential.hpp"
 #include "optimiser.hpp"
 #include "partial.hpp"
 #include "utils.hpp"
+#include "survival.svb.h"
 
 
 // [[Rcpp::export]]
-Rcpp::List fit_exp(arma::vec T, arma::vec delta, arma::mat X, double lambda,
-    double a_0,  double b_0, double a_omega, double b_omega, 
-    arma::vec m, arma::vec s, arma::vec g, int maxiter, bool verbose)
+Rcpp::List fit_partial(vec T, vec delta, mat X, double lambda, double a_0,
+    double b_0, vec m, vec s, vec g, int maxiter, double tol, bool verbose)
 {
-    arma::uword p = X.n_cols;
-    arma::uword n = X.n_rows;
-    
-    // initialisations
-    arma::vec m_old = arma::vec(p, arma::fill::zeros);
-    arma::vec s_old = arma::vec(p, arma::fill::zeros);
-    arma::vec g_old = arma::vec(p, arma::fill::zeros);
-    double a_old, b_old, omega;
-    double a = a_omega; double b = b_omega;
-    int n_delta = sum(delta);
-   
-    arma::vec P = init_P(X, m, s, g);
-    
-    for (int iter = 0; iter < maxiter; ++iter) {
+    uint p = X.cols();
 
-	m_old = m;  s_old = s;  g_old = g; a_old = a; b_old = b;
-
-	a = opt_exp_a(b, a_omega, b_omega, P, T, n_delta);
-	b = opt_exp_b(a, a_omega, b_omega, P, T, n_delta);
-
-	for (arma::uword j = 0; j < p; ++j) {
-	    arma::colvec x_j = X.col(j);
-	    omega = a / b;
-	    
-	    // remove g_j M(x_j, m_j, s_j) - (1 - g_j) from P_i
-	    P = rm_P(P, x_j, m(j), s(j), g(j));
-
-	    m(j) = opt_exp_mu(s(j), omega, lambda, P, T, delta, x_j);
-	    s(j) = opt_exp_sig(m(j), omega, lambda, P, T, delta, x_j);
-	    g(j) = opt_exp_gam(m(j), s(j), omega, lambda, a_0, b_0, 
-		    P, T, delta, x_j);
-
-	    // add in g_j M(x_j, m_j, s_j) - (1 - g_j) from P_i
-	    P = add_P(P, x_j, m(j), s(j), g(j));
-	}
-
-	if (sum(abs(m_old - m)) < 1e-6 && sum(abs(s_old - s)) < 1e-6 &&
-	    std::abs(a_old - a) < 1e-6 && std::abs(b_old - b) < 1e-6) {
-	    if (verbose)
-		Rcpp::Rcout << "Converged in " << iter << " iterations\n";
-	    break;
-	}
-    }
-    
-
-    return Rcpp::List::create(
-	Rcpp::Named("mu") = m,
-	Rcpp::Named("sigma") = s,
-	Rcpp::Named("gamma") = g,
-	Rcpp::Named("a") = a,
-	Rcpp::Named("b") = b
-    );
-}
-
-
-// [[Rcpp::export]]
-Rcpp::List fit_partial(arma::vec T, arma::vec delta, arma::mat X, 
-    double lambda, double a_0, double b_0, arma::vec m, arma::vec s, 
-    arma::vec g, int maxiter, double tol, bool verbose)
-{
-    arma::uword p = X.n_cols;
-
-    arma::uvec F = find(delta);		// indices of failure times
-    arma::vec FT = sort(T(F));		// failure times
-    std::vector<arma::uvec> R;
-
-    for (arma::uword i = 0; i < FT.size() - 1; ++i)
-	R.push_back(find(T < FT(i+1) && T >= FT(i)));
-
-    R.push_back(find(T >= FT(FT.size() - 1)));
+    std::vector<uint> delta_ord = order_delta(T, delta);
 
     // initialisations
-    arma::vec m_old, s_old, g_old;
-    arma::vec P = init_log_P(X, m, s, g);
+    vec m_old, s_old, g_old;
+    vec P = init_log_P(X, m, s, g);
 
     for (int iter = 0; iter < maxiter; ++iter) {
 	Rcpp::checkUserInterrupt();
@@ -95,28 +27,28 @@ Rcpp::List fit_partial(arma::vec T, arma::vec delta, arma::mat X,
 	// update old
 	m_old = m; s_old = s; g_old = g;
 
-	for (arma::uword j = 0; j < p; ++j) {
-	    arma::vec x_j = X.col(j);
+	for (uint j = 0; j < p; ++j) {
+	    vec x_j = X.col(j);
 
 	    rm_log_P(P, x_j, m(j), s(j), g(j));
 
-	    m(j) = opt_par_mu(s(j), lambda, R, F, P, x_j);
-	    s(j) = opt_par_sig(m(j), lambda, R, F, P, x_j);
-	    g(j) = opt_par_gam(m(j), s(j), a_0, b_0, lambda, R, F, P, x_j);
+	    m(j) = opt_par_mu( s(j), lambda, P, x_j, delta_ord);
+	    s(j) = opt_par_sig(m(j), lambda, P, x_j, delta_ord);
+	    g(j) = opt_par_gam(m(j), s(j), lambda, a_0, b_0, P, x_j, delta_ord);
 
 	    add_log_P(P, x_j, m(j), s(j), g(j));
 	    
 	    // check for overflow
-	    if (P.has_nan() || P.has_inf())
+	    if (!P.allFinite())
 		Rcpp::stop("Overflow error. Try rescaling X or using\
 			different starting values");
 
 	}
 
 	// check convergence
-	if (sum(abs(m - m_old)) < tol && 
-	    sum(abs(s - s_old)) < tol && 
-	    sum(abs(g - g_old)) < tol) {
+	if ((m - m_old).cwiseAbs().sum() < tol && 
+	    (s - s_old).cwiseAbs().sum() < tol && 
+	    (g - g_old).cwiseAbs().sum() < tol) {
 	    if (verbose)
 		Rcpp::Rcout << "Converged in " << iter << " iterations\n";
 	    return Rcpp::List::create(
@@ -135,43 +67,5 @@ Rcpp::List fit_partial(arma::vec T, arma::vec delta, arma::mat X,
 	Rcpp::Named("sigma") = s,
 	Rcpp::Named("gamma") = g
     );
-}
-
-
-// [[Rcpp::export]]
-Rcpp::List construct_risk_set(const arma::vec &T, const arma::vec &delta)
-{
-    arma::uvec F = find(delta);		// indices of failure times
-    arma::vec FT = sort(T(F));		// failure times
-    std::vector<arma::uvec> R;
-
-    for (arma::uword i = 0; i < FT.size() - 1; ++i)
-	R.push_back(find(T < FT(i+1) && T >= FT(i)));
-
-    R.push_back(find(T >= FT(FT.size() - 1)));
-
-    return Rcpp::List::create(
-	Rcpp::Named("F") = F,
-	Rcpp::Named("R") = R
-    );
-}
-
-
-// [[Rcpp::export]]
-double log_likelihood(const arma::vec &b, const arma::mat &X,
-	const std::vector<arma::uvec> &R, const arma::uvec F)
-{
-    arma::vec xb = X * b;
-    double a = max(xb);
-    double risk = 0.0;
-    double tot = sum(xb(F));
-
-    for (auto it = R.rbegin(); it != R.rend(); ++it) {
-	arma::uvec risk_set = (*it);
-	risk += sum(exp(xb(risk_set) - a));
-	tot -= (a + log(risk));
-    }
-
-    return tot;
 }
 
