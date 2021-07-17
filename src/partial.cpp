b0f76d6d0d2c21281306aa1adcf224bbeaa0a56e
mkomod <mkomodromos01@gmail.com>
2021-07-17 23:05:24 +0100

    fix: valid range of sigmas no longer 0, stability improvement

--- src/partial.cpp
+++ src/partial.cpp
@@ -11,17 +11,17 @@ struct kwargs {
 };
 
 
-static double pm(double mu, double sigma, const vec &P, const vec &x_j, 
+// [[Rcpp::export]]
+double pm(double mu, double sigma, const vec &P, const vec &x_j,
 	const std::vector<uint> &delta_ord) 
 {
     double s = 0.0;
     double t = 0.0;
     double a = 0.0;
 
-    // a = (P + log_normal_mgf(x_j, mu, sigma)).maxCoeff();
-	
-    uint icf = 0;
-    uint ilf = x_j.rows();
+    a = (P + log_normal_mgf(x_j, mu, sigma)).maxCoeff();
+    uint icf = 0;		// index of current failure
+    uint ilf = x_j.rows();	// index of last failure
 
     for (int i = delta_ord.size() - 1; i >= 0; --i) {
 	icf = delta_ord.at(i);
@@ -37,6 +37,7 @@ static double pm(double mu, double sigma, const vec &P, const vec &x_j,
     return t;
 }
 
+
 static double f_par_mu(double mu, void* args)
 {
     kwargs *arg = static_cast<kwargs *>(args);
@@ -49,9 +50,8 @@ static double f_par_mu(double mu, void* args)
     double t = pm(mu, sigma, P, x_j, delta_ord);
 
     double res = t +
-	lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2) * 0.5) +
-	lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) -
-	log(sigma);
+	lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2.0) * 0.5) +
+	lambda * mu * (1.0 - 2.0 * R::pnorm(- mu/sigma, 0, 1, 1, 0));
 
     return res;
 }
@@ -69,8 +69,8 @@ static double f_par_sig(double sigma, void* args)
     double t = pm(mu, sigma, P, x_j, delta_ord);
 
     double res = t +
-	lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2) * 0.5) +
-	lambda * mu * (1.0 - 2.0 * R::pnorm(- mu / sigma, 0, 1, 1, 0)) -
+	lambda * sigma * sqrt(2.0/PI) * exp(-pow(mu/sigma, 2.0) * 0.5) +
+	lambda * mu * (1.0 - 2.0 * R::pnorm(- mu/sigma, 0, 1, 1, 0)) -
 	log(sigma);
 
     return res;
@@ -92,13 +92,13 @@ double opt_par_sig(double mu, double lambda, const vec &P, const vec &x_j,
 	const std::vector<uint> &delta_ord)
 {
     kwargs args = { mu, 0.0, lambda, P, x_j, delta_ord };
-    double res = Brent_fmin(0.0, 10.0, f_par_sig, static_cast<void *>(&args), 1e-5);
+    double res = Brent_fmin(1e-3, 5.0, f_par_sig, static_cast<void *>(&args), 1e-5);
     return res;
 }
 
 
 // [[Rcpp::export]]
-double opt_par_gam(double mu, double sigma, double lambda, double a_0, double b_0, 
+double opt_par_gam(double mu, double sigma, double lambda, double a_0, double b_0,
     const vec &P, const vec &x_j, const std::vector<uint> &delta_ord)
 {
     double t = 0.0; 
@@ -107,8 +107,8 @@ double opt_par_gam(double mu, double sigma, double lambda, double a_0, double b_
     double a = 0.0; 
     double b = 0.0;
 
-    // a = std::min((P + log_normal_mgf(x_j, mu, sigma)).maxCoeff() / 5.0, 175.0);
-    // b = std::min(P.maxCoeff() / 5.0, 175.0);
+    a = std::min((P + log_normal_mgf(x_j, mu, sigma)).maxCoeff() / 5.0, 175.0);
+    b = std::min(P.maxCoeff() / 5.0, 175.0);
     uint icf = 0;
     uint ilf = x_j.rows();
 
@@ -120,7 +120,7 @@ double opt_par_gam(double mu, double sigma, double lambda, double a_0, double b_
 	    p += exp(P(j) - b);
 	}
 	
-	t += (a + log(s)) - (b  + log(p)) - mu * x_j(icf);
+	t += (a + log(s)) - (b + log(p)) - mu * x_j(icf);
 
 	ilf = icf;
     }
