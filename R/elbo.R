20c282121764b803579e1be45a45d8d10c8d6f62
mkomod <mkomodromos01@gmail.com>
2021-08-16 17:45:50 +0100

    rf: cleanup elbo

--- R/elbo.R
+++ R/elbo.R
@@ -9,6 +9,7 @@
 #' @export
 elbo <- function(Y, delta, X, fit, nrep=1e4)
 {
+    
     p <- ncol(X)
     m <- fit$m
     s <- fit$s
@@ -16,16 +17,17 @@ elbo <- function(Y, delta, X, fit, nrep=1e4)
     lambda <- fit$lambda
     a0 <- fit$a0
     b0 <- fit$b0
-    w <- a0 / (a0 + b0)
 
     res <- replicate(nrep, {
 	b. <- (runif(p) < g) * rnorm(p, m, s)
 	log_likelihood(b., X, Y, delta)
     }) - sum(
-	(1-g)*log(1 - g + 1e-18) - (1-g)*log(1 - w) +
-	g*log(g/w) - 0.5*g*log(2*pi*s^2) - g/2 -
-	g*log(lambda/2) + lambda*g*s*sqrt(2/pi)*exp(-(m^2)/(2*s^2)) + 
-	lambda*g*m*(1-2*pnorm(-m/s))
+	g * (lambda * s * sqrt(2/pi)*exp(-(m^2)/(2*s^2)) + 
+	     lambda * m * (1 - 2*pnorm(-m / s)) +
+	     log(sqrt(2) / (sqrt(pi) * s * lambda)) -
+	     0.5 -
+	     log(a0 / b0)) +
+	g * log(g) + (1-g)*log(1 - g + 1e-18)
     )
 
     return(list(mean=mean(res), sd=sd(res)))
