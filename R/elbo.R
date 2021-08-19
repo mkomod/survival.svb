c738c6528630ff8796ef5074a4b1d90d9a3fc49b
mkomod <mkomodromos01@gmail.com>
2021-08-19 12:43:55 +0100

    ft: return expected likelihood and kl(Q || Pi)

--- R/elbo.R
+++ R/elbo.R
@@ -9,7 +9,6 @@
 #' @export
 elbo <- function(Y, delta, X, fit, nrep=1e4)
 {
-    
     p <- ncol(X)
     m <- fit$m
     s <- fit$s
@@ -18,10 +17,11 @@ elbo <- function(Y, delta, X, fit, nrep=1e4)
     a0 <- fit$a0
     b0 <- fit$b0
 
-    res <- replicate(nrep, {
+    res.likelihood <- replicate(nrep, {
 	b. <- (runif(p) < g) * rnorm(p, m, s)
-	log_likelihood(b., X, Y, delta)
-    }) - sum(
+	log_likelihood(Y, delta, X, b.)
+    }) 
+    res.kl <- sum(
 	g * (lambda * s * sqrt(2/pi)*exp(-(m^2)/(2*s^2)) + 
 	     lambda * m * (1 - 2*pnorm(-m / s)) +
 	     log(sqrt(2) / (sqrt(pi) * s * lambda)) -
@@ -30,5 +30,11 @@ elbo <- function(Y, delta, X, fit, nrep=1e4)
 	g * log(g) + (1-g)*log(1 - g + 1e-18) - log(b0 / (a0 + b0))
     )
 
-    return(list(mean=mean(res), sd=sd(res)))
+    m.res.likelihood <- mean(res.likelihood)
+    s.res.likelihood <- sd(res.likelihood)
+
+    return(list(mean=m.res.likelihood - res.kl, 
+		sd=s.res.likelihood,
+		expected.likelihood=m.res.likelihood, 
+		kl.Q_Pi=res.kl))
 }
