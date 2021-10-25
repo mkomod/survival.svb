fed61f0529563bc21f3e17d9b22832416d930bc8
mkomod <mkomodromos01@gmail.com>
2021-10-25 11:23:53 +0100

    up: docs

--- R/elbo.R
+++ R/elbo.R
@@ -1,11 +1,30 @@
-#' Compute the evidence lower bound (ELBO) for the Cox model
+#' Compute the evidence lower bound (ELBO)
 #'
-#' @param Y failure times
-#' @param delta censoring indicator
-#' @param X deisgn matrix
-#' @param fit fit model
-#' @param nrep Monte Carlo samples
-#' @param center center the design matrix
+#' @param Y Failure times.
+#' @param delta Censoring indicator, 0: censored, 1: uncensored.
+#' @param X Design matrix.
+#' @param fit Fit model.
+#' @param nrep Number of Monte Carlo samples.
+#' @param center Should the design matrix be centered.
+#'
+#' @return Returns a list containing: \cr
+#' \item{mean}{The mean of the ELBO.}
+#' \item{sd}{The standard-deviation of the ELBO.}
+#' \item{expected.likelihood}{The expectation of the likelihood
+#' under the variational posterior.}
+#' \item{kl}{The KL between the variational posterior and prior.}
+#'
+#' @section Details:
+#' The evidence lower bound (ELBO) is a popular goodness of fit measure
+#' used in variational inference. Under the variational posterior the
+#' ELBO is given as
+#' \deqn{\text{ELBO} = E_{\tilde{\Pi}}[\log L_p(\beta; Y, X, \delta)] - \text{KL}(\tilde{\Pi} \| \Pi)}
+#' where \eqn{\tilde{\Pi}} is the variational posterior, \eqn{\Pi} is the prior,
+#' \eqn{L_p(\beta; Y, X, delta)} is Cox's partial likelihood. Intuitively,
+#' within the ELBO we incur a trade-off between how well we fit to the data
+#' (through the expectation of the log-partial-likelihood) and how close we
+#' are to our prior (in terms of KL divergence). Ideally we want the ELBO to be 
+#' as small as possible.
 #'
 #' @export
 elbo <- function(Y, delta, X, fit, nrep=1e4, center=TRUE)
--- R/svb_fit.R
+++ R/svb_fit.R
@@ -1,4 +1,4 @@
-#' Fit sparse variational Bayesian proportional hazards model.
+#' Fit sparse variational Bayesian proportional hazards models.
 #'
 #' @param Y Failure times.
 #' @param delta Censoring indicator, 0: censored, 1: uncensored.
@@ -6,19 +6,61 @@
 #' @param lambda Penalisation parameter, default: \code{lambda=0.5}.
 #' @param a0 Beta distribution parameter, default: \code{a0=1}.
 #' @param b0 Beta distribution parameter, default: \code{b0=ncol(X)}.
-#' @param mu.init Initial value for the means of the Gaussian component of the variational family (\eqn{\mu}), default taken from LASSO fit.
-#' @param s.init Initial value for the standard deviations of the Gaussian component of the variational family (\eqn{s}), default: \code{rep(0.05, ncol(X))}.
-#' @param g.init Initial value for the inclusion probability (\eqn{\gamma}), default: \code{rep(0.5, ncol(X))}.
+#' @param mu.init Initial value for the mean of the Gaussian component of 
+#' the variational family (\eqn{\mu}), default taken from LASSO fit.
+#' @param s.init Initial value for the standard deviations of the Gaussian 
+#' component of the variational family (\eqn{s}), default: 
+#' \code{rep(0.05, ncol(X))}.
+#' @param g.init Initial value for the inclusion probability (\eqn{\gamma}), 
+#' default: \code{rep(0.5, ncol(X))}.
 #' @param maxiter Maximum number of iterations, default: \code{1000}.
 #' @param tol Convergence tolerance, default: \code{0.001}.
-#' @param alpha The elastic-net mixing parameter used for initialising \code{mu.init}. When \code{alpha=1} the lasso penalty is used and \code{alpha=0} the ridge penalty, values between 0 and 1 give a mixture of the two penalties, default: \code{1}.
-#' @param center Center X prior to fitting, increases numerical stability, default: \code{TRUE}
+#' @param alpha The elastic-net mixing parameter used for initialising \code{mu.init}. 
+#' When \code{alpha=1} the lasso penalty is used and \code{alpha=0} the ridge 
+#' penalty, values between 0 and 1 give a mixture of the two penalties, default:
+#' \code{1}.
+#' @param center Center X prior to fitting, increases numerical stability, 
+#' default: \code{TRUE}
 #' @param verbose Print additional information: default: \code{TRUE}.
 #'
+#' @return Returns a list containing: \cr
+#' \item{beta_hat}{Point estimate for the coefficients \eqn{\beta}, taken as 
+#' the mean under the variational approximation 
+#' \eqn{\hat{\beta}_j = E_{\tilde{\Pi}} [ \beta_j ] = \gamma_j \mu_j}.}
+#' \item{inclusion_prob}{Posterior inclusion probabilities. Used to describe
+#' the posterior probability a coefficient is non-zero.}
+#' \eqn{\hat{\beta}_j = E_{\tilde{\Pi}} [ \beta_j ] = \gamma_j \mu_j}.}
+#' \item{m}{Final value for the means of the Gaussian component of the variational 
+#' family \eqn{\mu}.}
+#' \item{s}{Final value for the standard deviation of the Gaussian component of 
+#' the variational family \eqn{s}.}
+#' \item{g}{Final value for the inclusion probability (\eqn{\gamma}.}
+#' \item{lambda}{Value of lambda used.}
+#' \item{a0}{Value of \eqn{\alpha_0} used.}
+#' \item{b0}{Value of \eqn{\beta_0} used.}
+#' \item{converged}{Describes whether the algorithm converged.}
+#'
+#' @section Details:
+#' Rather than compute the posterior using MCMC, we turn to approximating it
+#' using variational inference. Within variational inference we re-cast
+#' Bayesian inference as an optimisation problem, where we minimize the 
+#' Kullback-Leibler (KL) divergence between a family of tractable distributions 
+#' and the posterior, \eqn{\Pi}. \cr \cr In our case we use a mean-field variational 
+#' family,
+#' \deqn{Q = \{ \prod_{j=1}^p \gamma_j N(\mu_j, s_j^2) + (1 - \gamma_j) \delta_0 \}}
+#' where \eqn{\mu_j} is the mean and \eqn{s_j} the std. dev for the Gaussian 
+#' component, \eqn{\gamma_j} the inclusion probabilities, \eqn{\delta_0} a Dirac mass 
+#' at zero and \eqn{p} the number of coefficients.\cr \cr The components of the
+#' variational family (\eqn{\mu, s, \gamma}) are then optimised by minimizing the 
+#' Kullback-Leibler divergence between the variational family and the posterior,
+#' \deqn{\tilde{\Pi} = \arg \min \text{KL}(Q \| \Pi).} We use co-ordinate ascent
+#' variational inference (CAVI) to solve the above optimisation problem. \cr \cr
+#' 
+#'
 #' @examples
-#' \dontrun{
+#'
 #' n <- 200                        # number of sample
-#' p <- 1000                       # number of features
+#' p <- 500                        # number of features
 #' s <- 10                         # number of non-zero coefficients
 #' censoring_lvl <- 0.4            # degree of censoring
 #' 
@@ -42,7 +84,7 @@
 #' legend("topleft", legend=c(expression(beta), expression(hat(beta))),
 #'        pch=c(8, 20), col=c(1, 2))
 #' plot(f$g, main="Inclusion Probabilities", ylab=expression(gamma))
-#' }
+#'
 #' @export
 svb.fit <- function(Y, delta, X, lambda=0.5, a0=1, b0=ncol(X),
     mu.init=NULL, s.init=rep(0.05, ncol(X)), g.init=rep(0.5, ncol(X)), 
@@ -76,5 +118,6 @@ svb.fit <- function(Y, delta, X, lambda=0.5, a0=1, b0=ncol(X),
     res <- fit_partial(Y, delta, X, lambda, a0, b0,
 	mu.init, s.init, g.init, maxiter, tol, verbose)
 
-    return(c(res, beta_hat=res$m * res$g, lambda=lambda, a0=a0, b0=b0))
+    return(c(res, beta_hat=res$m * res$g, inclusion_prob=res$g, 
+	     lambda=lambda, a0=a0, b0=b0))
 }
